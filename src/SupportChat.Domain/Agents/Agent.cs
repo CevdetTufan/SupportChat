33a9546f5c6f2e5d237025f85c26ebe9dba674ff
Cevdet Tufan <cevdet.tufan@hotmail.com>
2025-07-16 20:54:08 +0300

    #2 - Agent domain created.

---
+++ src/SupportChat.Domain/Agents/Agent.cs
@@ -0,0 +1,10 @@
+﻿namespace SupportChat.Domain.Agents;
+
+public class Agent
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public Seniority Seniority { get; set; }
+	public double Efficiency { get; set; }
+	public AgentStatus Status { get; set; }
+}
---
+++ src/SupportChat.Domain/Agents/AgentStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportChat.Domain.Agents;
+
+public enum AgentStatus
+{
+	Active = 1,
+	FinishingShift,
+	Inactive
+}
